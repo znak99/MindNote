7d023d7c1e31a8c7805b097a1c321a88c1421cd4
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-18 14:27:02 +0900

    Connect home - detail page

--- diary/urls.py
+++ diary/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("dairy/", views.page_list),
-    # path("dairy/info/", views.info),
-    # path("dairy/write/", views.page_create),
-    # path("dairy/page/<int:page_id>/", views.page_detail),
-    # path("dairy/page/<int:page_id>/edit/", views.page_update),
-    # path("dairy/page/<int:page_id>/delete/", views.page_delete),
+    path("diary/", views.page_list),
+    # path("diary/info/", views.info),
+    # path("diary/write/", views.page_create),
+    path("diary/page/<int:page_id>/", views.page_detail),
+    # path("diary/page/<int:page_id>/edit/", views.page_update),
+    # path("diary/page/<int:page_id>/delete/", views.page_delete),
 ]
--- diary/views.py
+++ diary/views.py
@@ -6,3 +6,8 @@ def page_list(request):
     pages = Page.objects.all()
     context = {"object_list": pages}
     return render(request, 'diary/index.html', context=context)
+
+def page_detail(request, page_id):
+    page = Page.objects.get(id=page_id)
+    context = {"object": page}
+    return render(request, 'diary/page_detail.html', context=context)
