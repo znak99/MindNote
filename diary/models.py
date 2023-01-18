c5706ce863f1354198ba6c1b15fc9474fe8d6b0f
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-18 12:04:54 +0900

    Create models

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feeling = models.CharField(max_length=80)
+    score = models.IntegerField()
+    dt_created = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
