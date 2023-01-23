7f2ded59ef948bf85e43d2c04cf17c57ad660be6
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-24 01:59:16 +0900

    Add seed data

---
+++ diary/validate_data.py
@@ -0,0 +1,10 @@
+from .models import Page
+import random
+
+def validate_pages():
+    pages = Page.objects.all()
+
+    for page in pages:
+        if page.score < 0 or page.score > 10:
+            page.score = random.randint(0, 10)
+            page.save()
