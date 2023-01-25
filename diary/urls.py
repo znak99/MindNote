9f8777c6643c451c9ffd79662421f208c211b4ed
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-25 14:22:56 +0900

    Change functional-delete-view to Class-generic-view

--- diary/urls.py
+++ diary/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('diary/write/', views.PageCreateView.as_view(), name='page-create'),
     path('diary/page/<int:page_id>/', views.PageDetailView.as_view(), name='page-detail'),
     path('diary/page/<int:page_id>/edit/', views.PageUpdateView.as_view(), name='page-update'),
-    path('diary/page/<int:page_id>/delete/', views.page_delete, name='page-delete'),
+    path('diary/page/<int:page_id>/delete/', views.PageDeleteView.as_view(), name='page-delete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse
 from .models import Page
 from .forms import PageForm
@@ -74,13 +74,21 @@ class PageUpdateView(UpdateView):
     def get_success_url(self):
         return reverse('page-detail', kwargs={'page_id': self.object.id})
 
-def page_delete(request, page_id):
-    object = Page.objects.get(id=page_id)
-    if request.method == 'POST':
-        object.delete()
-        return redirect('page-list')
-    else:
-        return render(request, 'diary/page_confirm_delete.html', {'object': object})
+# def page_delete(request, page_id):
+#     object = Page.objects.get(id=page_id)
+#     if request.method == 'POST':
+#         object.delete()
+#         return redirect('page-list')
+#     else:
+#         return render(request, 'diary/page_confirm_delete.html', {'object': object})
+
+class PageDeleteView(DeleteView):
+    model = Page
+    template_name = 'diary/page_confirm_delete.html'
+    pk_url_kwarg = 'page_id'
+
+    def get_success_url(self):
+        return reverse('page-list')
 
 def index(request):
     return render(request, 'diary/index.html')
