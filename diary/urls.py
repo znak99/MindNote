5ea3fe461fb66ab3c9ac53275a0c23380102ef4f
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-25 14:10:53 +0900

    Change functional-update-view to Class-generic-view

--- diary/urls.py
+++ diary/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('diary/info/', views.info, name='info'),
     path('diary/write/', views.PageCreateView.as_view(), name='page-create'),
     path('diary/page/<int:page_id>/', views.PageDetailView.as_view(), name='page-detail'),
-    path('diary/page/<int:page_id>/edit/', views.page_update, name='page-update'),
+    path('diary/page/<int:page_id>/edit/', views.PageUpdateView.as_view(), name='page-update'),
     path('diary/page/<int:page_id>/delete/', views.page_delete, name='page-delete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.urls import reverse
 from .models import Page
 from .forms import PageForm
@@ -54,16 +54,25 @@ class PageCreateView(CreateView):
     def get_success_url(self):
         return reverse('page-detail', kwargs={'page_id': self.object.id})
 
-def page_update(request, page_id):
-    object = Page.objects.get(id=page_id)
-    if request.method == 'POST':
-        form = PageForm(request.POST, instance=object)
-        if form.is_valid():
-            form.save()
-            return redirect('page-detail', page_id=object.id)
-    else:
-        form = PageForm(instance=object)
-    return render(request, 'diary/page_form.html', {'form': form})
+# def page_update(request, page_id):
+#     object = Page.objects.get(id=page_id)
+#     if request.method == 'POST':
+#         form = PageForm(request.POST, instance=object)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('page-detail', page_id=object.id)
+#     else:
+#         form = PageForm(instance=object)
+#     return render(request, 'diary/page_form.html', {'form': form})
+
+class PageUpdateView(UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+    pk_url_kwarg = 'page_id'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.object.id})
 
 def page_delete(request, page_id):
     object = Page.objects.get(id=page_id)
