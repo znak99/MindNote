d40b58f8e42bd6270fdd7a5c7802de7b2d9557af
znak99 <xhahekcldhkznak0113@gmail.com>
2023-01-25 13:08:13 +0900

    Change functional-create-view to Class-generic-view

--- diary/views.py
+++ diary/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Page
 from .forms import PageForm
 
@@ -22,15 +24,23 @@ def page_detail(request, page_id):
     context = {"object": page}
     return render(request, 'diary/page_detail.html', context=context)
 
-def page_create(request):
-    if request.method == 'POST':
-        form = PageForm(request.POST)
-        if form.is_valid():
-            new_page = form.save()
-            return redirect('page-detail', page_id=new_page.id)
-    else:
-        form = PageForm()
-    return render(request, 'diary/page_form.html', {'form': form})
+# def page_create(request):
+#     if request.method == 'POST':
+#         form = PageForm(request.POST)
+#         if form.is_valid():
+#             new_page = form.save()
+#             return redirect('page-detail', page_id=new_page.id)
+#     else:
+#         form = PageForm()
+#     return render(request, 'diary/page_form.html', {'form': form})
+
+class PageCreateView(CreateView):
+    model = Page
+    form_class = PageForm
+    template_name = 'diary/page_form.html'
+
+    def get_success_url(self):
+        return reverse('page-detail', kwargs={'page_id': self.object.id})
 
 def page_update(request, page_id):
     object = Page.objects.get(id=page_id)
